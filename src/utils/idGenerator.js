f9568cd7beecaeb8d91e6d01278fa18df538dc6c
zahidmahfudz <zaemahfudz@gmail.com>
2025-09-13 21:23:33 +0700

    fixing err_require_esm 2 : downversion uuid

--- src/utils/idGenerator.js
+++ src/utils/idGenerator.js
@@ -1,5 +1,5 @@
-import { v4 as uuidv4 } from 'uuid';
-
-export function generateUserId() {
-    return `usr-${uuidv4()}`;
+const { v4: uuidv4 } = require('uuid');
+function generateUserId() {
+  return `usr-${uuidv4()}`;
 }
+module.exports = { generateUserId };
